0291fb85a8636de368d9bc213f1bccb1be94fe75
Phillip Schichtel <phillip@schich.tel>
2021-12-17 01:29:47 +0100

    update keycloak script

--- keycloak/run-keycloak-container.sh
+++ keycloak/run-keycloak-container.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+host_ip="$(ip addr show docker0 | grep -Po 'inet \K[\d.]+')"
 docker run --rm --name keycloak \
-    -v $PWD/target/keycloak_http_webhook_provider-jar-with-dependencies.jar:/opt/jboss/keycloak/standalone/deployments/http_webhook_provider.jar \
-    -v $PWD/keycloak/standalone-ha.xml:/opt/jboss/keycloak/standalone/configuration/standalone-ha.xml \
-    -v $PWD/keycloak/startup-script.sh:/opt/jboss/startup-scripts/startup-script.sh \
-    -e KEYCLOAK_USER=admin \
-    -e KEYCLOAK_PASSWORD=admin \
-    -p 8080:8080 \
-    --add-host host.docker.internal:$(ip addr show docker0 | grep -Po 'inet \K[\d.]+') \
+    -v "$PWD/target/keycloak_http_webhook_provider.jar:/opt/jboss/keycloak/standalone/deployments/http_webhook_provider.jar" \
+    -v "$PWD/keycloak/startup-script.sh:/opt/jboss/startup-scripts/startup-script.sh" \
+    -e KEYCLOAK_USER="admin" \
+    -e KEYCLOAK_PASSWORD="admin" \
+    -e KEYCLOAK_WEBHOOK_URL="http://$host_ip:5000" \
+    -p '8080:8080' \
     "$@" \
-    jboss/keycloak
+    jboss/keycloak:15.0.2
