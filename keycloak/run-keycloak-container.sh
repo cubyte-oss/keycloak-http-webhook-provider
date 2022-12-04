af1d59f64ca8089fbff416f943874c97722b533a
Phillip Schichtel <phillip@schich.tel>
2022-12-04 15:37:49 +0100

    update the keycloak run script and add a debug config

--- keycloak/run-keycloak-container.sh
+++ keycloak/run-keycloak-container.sh
@@ -4,9 +4,15 @@ host_ip="$(ip addr show docker0 | grep -Po 'inet \K[\d.]+')"
 docker run --rm --name keycloak \
     -v "$PWD/target/keycloak_http_webhook_provider.jar:/opt/jboss/keycloak/standalone/deployments/http_webhook_provider.jar" \
     -v "$PWD/keycloak/startup-script.sh:/opt/jboss/startup-scripts/startup-script.sh" \
+    -e DB_VENDOR=h2 \
     -e KEYCLOAK_USER="admin" \
     -e KEYCLOAK_PASSWORD="admin" \
     -e KEYCLOAK_WEBHOOK_URL="http://$host_ip:5000" \
+    -e KEYCLOAK_STATISTICS=db,http \
+    -e DEBUG=true \
+    -e DEBUG_PORT='*:8787' \
     -p '8080:8080' \
+    -p '8787:8787' \
+    -p '9990:9990' \
     "$@" \
-    jboss/keycloak:15.0.2
+    quay.io/keycloak/keycloak:17.0.1-legacy
