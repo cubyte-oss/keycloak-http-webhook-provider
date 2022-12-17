b45f599375c9d4d9630358387ffb0bc5d2714699
Phillip Schichtel <phillip@schich.tel>
2022-12-18 00:32:51 +0100

    switch to quarkus and version 20

--- keycloak/run-keycloak-container.sh
+++ keycloak/run-keycloak-container.sh
@@ -1,18 +1,46 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 host_ip="$(ip addr show docker0 | grep -Po 'inet \K[\d.]+')"
-docker run --rm --name keycloak \
-    -v "$PWD/target/keycloak_http_webhook_provider.jar:/opt/jboss/keycloak/standalone/deployments/http_webhook_provider.jar" \
-    -v "$PWD/keycloak/startup-script.sh:/opt/jboss/startup-scripts/startup-script.sh" \
-    -e DB_VENDOR=h2 \
-    -e KEYCLOAK_USER="admin" \
-    -e KEYCLOAK_PASSWORD="admin" \
-    -e KEYCLOAK_WEBHOOK_URL="http://$host_ip:5000" \
-    -e KEYCLOAK_STATISTICS=db,http \
-    -e DEBUG=true \
-    -e DEBUG_PORT='*:8787' \
-    -p '8080:8080' \
-    -p '8787:8787' \
-    -p '9990:9990' \
-    "$@" \
-    quay.io/keycloak/keycloak:18.0.2-legacy
+
+id="$(
+  docker run -d --rm --name keycloak \
+      -v "$PWD/target/keycloak_http_webhook_provider.jar:/opt/keycloak/providers/http_webhook_provider.jar" \
+      -e DEBUG_PORT='*:8787' \
+      -e KEYCLOAK_ADMIN="admin" \
+      -e KEYCLOAK_ADMIN_PASSWORD="admin" \
+      -e KEYCLOAK_WEBHOOK_URL="http://$host_ip:5000" \
+      -p '8080:8080' \
+      -p '8787:8787' \
+      "$@" \
+      quay.io/keycloak/keycloak:20.0.2-0 \
+      start-dev \
+      --debug \
+      --db dev-file \
+      --hostname-url http://localhost:8080/auth \
+      --hostname-strict=false \
+      --proxy=edge \
+      --http-enabled true \
+      --http-relative-path /auth
+)"
+
+add_http_listener() {
+    for i in {1..10}; do
+        curl --silent --head --fail http://localhost:8080/auth && \
+        docker exec "$id" /opt/keycloak/bin/kcadm.sh update events/config -s eventsListeners+=http_webhook \
+            --no-config --server http://localhost:8080/auth --user admin --password admin --realm master && \
+            exit 0
+        sleep 2
+    done
+    exit 1
+}
+
+finish() {
+  docker kill "$id"
+}
+trap finish EXIT
+
+add_http_listener &
+
+docker container logs -f "$id"
